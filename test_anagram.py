82d4532f5c511c7861ddec670e657ce978770c18
NileI <niletingham@gmail.com>
2026-01-14 15:40:33 +0000

    Break the test!

--- test_anagram.py
+++ test_anagram.py
@@ -9,5 +9,8 @@ class TestAnagramDetector(unittest.TestCase):
     def test_not_anagrams(self):
         self.assertFalse(are_anagrams("hello","world"))
 
+    def test_ignores_case(self):
+        self.assertTrue(are_anagrams("Listen","Silent"))
+
 if __name__ == "__main__":
     unittest.main()
